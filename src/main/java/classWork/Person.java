228992d8464290faa20459562af44aa7da0b9a8c
Dmitriy <desant74268@yandex.ru>
2020-06-08 21:36:42 +0300

    ClassWork

---
+++ src/main/java/classWork/Person.java
@@ -0,0 +1,19 @@
+package classWork;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@AllArgsConstructor
+@Getter
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+
+//    @Override
+//    public String toString() {
+//        return  name + " ";
+//    }
+}
